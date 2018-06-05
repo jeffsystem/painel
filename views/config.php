fe0eddf5de42e5b37e9028a376bed8063b19fb44
Jeferson Trindade <contato@jtpweb.com.br>
2018-06-05 00:05:56 -0300

    iniciando layout pagina de configuração

--- views/config.php
+++ views/config.php
@@ -15,8 +15,32 @@ require_once('../sidebar.php');
 <div class="content-wrapper">
 	<section class="content">
 		<div class="row">
-			<div class="col-md-12 col-sm-6 col-xs-12">
-				Cadastrar dados da empresa <br />(CNPJ, ATIVIDADE, E-MAIL,NOME DA EMPRESA, HORARIO FUNCIONAMENTO)
+			<div class="col-md-6 col-sm-6 col-xs-12">
+					<div class="box box-primary">
+						<div class="box-header with-border">
+							<h3 class="box-title">Cadastrar informações</h3>
+						</div>
+						<div class="box-body">
+
+						<div class="form-group">
+							<label for="exampleInputEmail1">CNPJ</label>
+							<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+						</div>
+
+						<div class="form-group">
+							<label for="exampleInputPassword1">Password</label>
+							<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+						</div>
+
+						<div class="form-group">
+							<label for="exampleInputFile">Logo</label>
+							<input type="file" id="exampleInputFile">
+						</div>
+
+					<div class="box-footer">
+						<button type="submit" class="btn btn-success pull-right">Cadastrar</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
